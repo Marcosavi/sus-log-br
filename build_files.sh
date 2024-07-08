5055b54fb87a73531d35323c574e37e4513126be
Marcosavi <marcoscsavi@gmail.com>
2024-07-08 14:16:45 -0300

    complete new build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,20 +1,2 @@
-#!/bin/bash
-
-echo "BUILD START"
-
-# Use the default Python available on Vercel, typically Python 3
-python -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
-python manage.py collectstatic --noinput
-
-echo "BUILD END"
-
-# [optional] Start the application here
-# python manage.py runserver
+python3.9 manage.py collectstatic --noinput
