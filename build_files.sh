3cbcfb33034115a0c370a094c60fbbb64be58b4f
Marcosavi <marcoscsavi@gmail.com>
2024-07-08 14:06:27 -0300

    commands sh

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Add the directory to PATH
+# Ensure /python312/bin is in the PATH
 export PATH=$PATH:/python312/bin
 
-# Install dependencies
-pip install -r requirements.txt
+# Install SQLite development libraries
+apk add --no-cache sqlite-libs sqlite-dev
+
+# Ensure pip is available and install dependencies
+python3.12 -m ensurepip
+pip3.12 install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3.12 manage.py collectstatic --noinput
 
 # Apply database migrations
-python manage.py migrate
+python3.12 manage.py migrate
